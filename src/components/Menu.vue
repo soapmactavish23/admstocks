ce3f166502e25899e8434bc1003de3c56e4590e8
unknown <ricknogueira1231@gmail.com>
2023-04-19 13:37:35 -0300

    Estilizando o Menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -3,6 +3,9 @@
   <nav class="conteudo">
     <ul>
       <router-link to="/">
+        <img src="./../assets/fullstack.png" width="100" />
+      </router-link>
+      <router-link to="/">
         <li>Home</li>
       </router-link>
       <router-link to="/about">
@@ -22,4 +25,17 @@
 export default {};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.conteudo {
+  ul {
+    display: flex;
+    list-style: none;
+    justify-content: space-around;
+    align-items: center;
+    li {
+      margin-left: 1%;
+      width: 100px;
+    }
+  }
+}
+</style>
