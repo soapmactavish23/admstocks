4ef1ccf1845bf0aa03b4ffb4c826e0b91a63d103
unknown <ricknogueira1231@gmail.com>
2023-04-19 13:18:40 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import Primevue from "primevue/config";
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.use(Primevue);
+app.mount("#app");
