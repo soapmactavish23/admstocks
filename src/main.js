cd59d0dabc93d67998c45d11c81c8e34aa1f2ece
unknown <ricknogueira1231@gmail.com>
2023-04-20 16:47:52 -0300

    v1

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 import "primeflex/primeflex.css";
 import "primevue/resources/themes/lara-light-indigo/theme.css";
 import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
 
 import { createApp } from "vue";
 
@@ -15,6 +16,13 @@ import store from "./store";
 import Primevue from "primevue/config";
 import Card from "primevue/card";
 import Button from "primevue/button";
+import Panel from "primevue/panel";
+import Datatable from "primevue/datatable";
+import Toolbar from "primevue/toolbar";
+import InputText from "primevue/inputtext";
+import Column from "primevue/column";
+import ColumnGroup from "primevue/columngroup"; // optional
+import Row from "primevue/row";
 
 const app = createApp(App);
 app.use(store);
@@ -23,5 +31,12 @@ app.use(Primevue);
 
 app.component("Card", Card);
 app.component("Button", Button);
+app.component("Panel", Panel);
+app.component("Datatable", Datatable);
+app.component("Toolbar", Toolbar);
+app.component("InputText", InputText);
+app.component("Column", Column);
+app.component("Row", Row);
+app.component("ColumnGroup", ColumnGroup);
 
 app.mount("#app");
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,9 +1,47 @@
 <template>
-  <h1>Produtos</h1>
+  <Panel header="Produtos">
+    <Toolbar>
+      <template #start>
+        <Button label="Novo" class="p-button-success" icon="pi pi-plus" />
+      </template>
+      <template #end>
+        <span class="p-input-icon-left">
+          <i class="pi pi-search" />
+          <InputText
+            v-model="filters['global'].value"
+            placeholder="Pesquisar..."
+          />
+        </span>
+      </template>
+    </Toolbar>
+    <DataTable :value="products" tableStyle="min-width: 50rem">
+      <Column field="id" header="id"></Column>
+    </DataTable>
+  </Panel>
 </template>
 
 <script>
-export default {};
+import { FilterMatchMode } from "primevue/api";
+
+import { products } from "../data/data";
+export default {
+  data() {
+    return {
+      products,
+      filters: {},
+    };
+  },
+  created() {
+    this.initFilters();
+  },
+  methods: {
+    initFilters() {
+      this.filters = {
+        global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+      };
+    },
+  },
+};
 </script>
 
 <style></style>
