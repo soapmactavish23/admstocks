d6b1029c3a77a265dd439517a456802fee0d4095
Henrick <ricknogueira1231@gmail.com>
2023-05-04 20:43:23 -0300

    novo produto

--- src/views/products/ProductFormView.vue
+++ src/views/products/ProductFormView.vue
@@ -1,9 +1,42 @@
 <template>
-  <h1>Novo produto</h1>
+  <div class="md:col-8 md:col-offset-2">
+    <form action="">
+      <Card class="text-center">
+        <template #title>
+          <h3>Novo Produto</h3>
+        </template>
+        <template #content>
+          <div class="p-fluid text-left">
+            <div class="field">
+              <label for="description">Descrição</label>
+              <InputText id="description" placeholder="Digite a descrição" required />
+            </div>
+            <div class="field">
+              <label for="quantity">Quantidade</label>
+              <InputText id="quantity" placeholder="Digite a quantidade" type="number" required />
+            </div>
+            <div class="field">
+              <label for="price">Preço</label>
+              <InputText id="price" placeholder="Digite o preço" type="number" required />
+            </div>
+          </div>
+        </template>
+        <template #footer>
+          <Button icon="pi pi-check" label="Salvar" class="p-button-success" @click="send" />
+        </template>
+      </Card>
+    </form>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    send() {
+      
+    }
+  }
+};
 </script>
 
 <style></style>
